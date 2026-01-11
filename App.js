592473fabdf4cfa08b65b40aa06feeb2a5e999ef
Saikiran <saikiran@Tradelabs-MacBook-Pro.local>
2026-01-11 13:58:11 +0530

    Episode2Complete

--- App.js
+++ App.js
@@ -9,12 +9,14 @@
         </div>
         </div>
          */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
 [  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
+    React.createElement("h1", { id: "heading" }, "This is Namaste React"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
